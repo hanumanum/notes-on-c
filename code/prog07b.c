d380c0e8ad797894996954d9e790a07d3554c729
Armen Badalian <armenbadal@gmail.com>
2014-04-21 10:48:29 +0400

    Ch 7, b

--- code/prog07b.c
+++ code/prog07b.c
@@ -18,16 +18,15 @@ double distance( struct point a, struct point b )
 
 double length( struct point ps[], int nm )
 {
-  if( nm == 1 ) return 0L;
+  if( nm == 1 ) return 0.0;
   return distance( ps[nm-1], ps[nm-2] ) + length( ps, nm-1 );
 }
 
 int main()
 {
-  struct point points[] = { {0,0}, {1,1}, {2,2} };
-  double r = length( points, 3 );
-  printf( ">> %lf\n", r );
-
+  struct point points[] = { {0,0}, {1,1}, {2,2}, {3,3} };
+  double res = length( points, 4 );
+  printf( ">> %lf\n", res );
   return 0;
 }
 
