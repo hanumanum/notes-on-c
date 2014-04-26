f1db29365fa5ca318cc4784b03b43ece9b7a6460
Armen Badalian <armenbadal@gmail.com>
2014-04-26 16:15:02 +0400

    Summary
    Description

--- code/prog06b.c
+++ code/prog06b.c
@@ -1,12 +1,20 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 14, 2014
+ *      Author: abadalya
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
 
+/**/
 struct node {
   double key;
   struct node* next;
 };
 
+/**/
 struct node* create_node( double k )
 {
   struct node* res = malloc(sizeof(struct node));
@@ -15,74 +23,62 @@ struct node* create_node( double k )
   return res;
 }
 
-void destroy_node( struct node* n )
-{
-  free( n );
-}
-
-struct node* add_front( double k, struct node* list )
+/**/
+void add_to_front( double k, struct node** list )
 {
   struct node* no = create_node( k );
-  no->next = list;
-  return no;
+  no->next = *list;
+  *list = no;
 }
 
-struct node* add_back( double k, struct node* list )
+/**/
+double remove_from_front( struct node** list )
 {
-  if( list == NULL )
-    return create_node( k );
+  struct node* no = *list;
+  *list = (*list)->next;
+  double res = no->key;
+  free( no );
+  return res;
+}
 
-  if( list->next == NULL )
-    list->next = create_node( k );
+/**/
+double remove_from_back( struct node** list )
+{
+  return 0.0;
+}
+
+/**/
+void add_to_back( double k, struct node** list )
+{
+  if( NULL == *list )
+    *list = create_node( k );
   else
-    add_back( k, list->next );
-
-  return list;
+    add_to_back( k, &((*list)->next) );
 }
 
-/* ?? */
-void add_back_2( double k, struct node* list )
-{
-  struct node* p = list;
-  while( p->next != NULL ) {
-    p = p->next;
-  }
-  // TODO
-}
-
-void print_list_rec( struct node* list )
+/**/
+void print_list( struct node* list )
 {
   if( list != NULL ) {
-    printf( "%lf ", list->key ); /* արտածել գլխի բանալին */
-    print_list_rec( list->next ); /* արտածել պոչը */
+    printf( "%lf ", list->key );
+    print_list( list->next );
   }
 }
 
-void print_list_iter( struct node* list )
-{
-  while( list != NULL ) {
-    printf( "%lf ", list->key ); /* արտածել բանալին */
-	list = list->next; /* անցնել հաջորդ հանգույցին */
-  }
-}
-
-
+/**/
 int main()
 {
-  struct node* e0 = NULL;
-  e0 = add_front( 5.6, e0 );
-  e0 = add_front( 3.4, e0 );
-  e0 = add_front( 1.2, e0 );
+  struct node* e1 = NULL;
+  add_to_front( 5.6, &e1 );
+  add_to_front( 3.4, &e1 );
+  add_to_front( 1.2, &e1 );
+  print_list( e1 );
 
-  print_list_rec( e0 );
-  putchar( '\n' );
-  print_list_iter( e0 );
-  putchar( '\n' );
-
-  e0 = add_back( 7.8, e0 );
-  print_list_iter( e0 );
-  putchar( '\n' );
-  
+  struct node* e2 = NULL;
+  add_to_back( 5.6, &e2 );
+  add_to_back( 3.4, &e2 );
+  add_to_back( 1.2, &e2 );
+  print_list( e2 );
 
   return 0;
 }
