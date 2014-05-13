13e485952cd51586f51139f208df784b6cc3069f
Armen Badalian <armenbadal@gmail.com>
2014-05-13 18:34:51 +0400

    New text and code

---
+++ code/ni.h
@@ -0,0 +1,8 @@
+
+#ifndef NUMERIC_INTEGRAL
+#define NUMERIC_INTEGRAL
+
+extern double integral( double(*f)(double), double, double );
+
+#endif
+
--- code/numint.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef NUMERIC_INTEGRAL_H
-#define NUMERIC_INTEGRAL_H
-
-extern double integral( double(*)(double), double, double );
-
-#endif
-
